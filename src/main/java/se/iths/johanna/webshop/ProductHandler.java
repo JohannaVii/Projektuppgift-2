f86a44324ac2d7dc1b1d22ec04493cc431a88e4a
Johannavii <jh.viippa@gmail.com>
2025-10-08 19:43:02 +0200

    Commit

--- src/main/java/se/iths/johanna/webshop/ProductHandler.java
+++ src/main/java/se/iths/johanna/webshop/ProductHandler.java
@@ -1,7 +1,11 @@
 package se.iths.johanna.webshop;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 // Klass som hanterar produkter och filer
 public class ProductHandler {
@@ -23,6 +27,27 @@ public class ProductHandler {
             }
         } catch (IOException e) {
             System.out.println("Error - Fel uppstod vid sparning: " + e.getMessage());
+        }
+    }
+
+    public static List<Product> loadProducts() {
+        List<Product> products = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("products.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(";");
+
+                String category = parts[4];
+                int articleNumber = Integer.parseInt(parts[0]);
+                String description = part[3];
+
+                Product product = new CustomProduct(category, articleNumber, title, price, description);
+                products.add(product);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Filen kunde inte läsas - Försök igen!");
         }
     }
 }
