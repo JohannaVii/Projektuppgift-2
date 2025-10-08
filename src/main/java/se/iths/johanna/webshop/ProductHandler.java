ad256ba8aae329d1012851f280fd792087827ba2
Johannavii <jh.viippa@gmail.com>
2025-10-08 19:01:20 +0200

    Commit

--- src/main/java/se/iths/johanna/webshop/ProductHandler.java
+++ src/main/java/se/iths/johanna/webshop/ProductHandler.java
@@ -7,12 +7,18 @@ import java.io.IOException;
 public class ProductHandler {
 
     public static void saveProducts(List<Product> catalog) {
-        try {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter())) {
             FileWriter writer = new FileWriter("products.txt");
             BufferedWriter bw = new BufferedWriter(writer);
 
             for (Product product : catalog) {
-                bw.write(product.toString());
+                String line = product.getArticleNumber() + ";" +
+                        product.getTitle() + ";" +
+                        product.getPrice() + ";" +
+                        product.getDescription() + ";" +
+                        product.category();
+
+                bw.write(line);
                 bw.newLine();
             }
         } catch (IOException e) {
